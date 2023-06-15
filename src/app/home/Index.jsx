0db8215154aefdfb9a528b05e77eaffe54b8f5ad
pedrogas <srpj4321@gmail.com>
2023-06-15 16:20:25 -0300

    api

--- src/app/home/Index.jsx
+++ src/app/home/Index.jsx
@@ -7,15 +7,25 @@ import Statistics from './Statistics'
 import Footer from './Footer'
 import '../../css/home/home.css'
 import { useState } from 'react'
+import { METHODS } from 'http'
 
 export default function Index() {
 
   const [links, setLinks] = useState([
     {url: 'https://www.wappalyzer.com/upgraded/?utm_source=upgraded&utm_medium', shortUrl: 'https://rel.ink/k4Pixy'},
-    {url: 'https://www.wappalyzer.com/upgraded/?utm_source=upgraded&utm_medium', shortUrl: 'https://rel.ink/k4Pixo'}
+    {url: 'https://www.wappalyzer.com/upgraded/?utm_source=upgraded&utm_medium', shortUrl: 'https://rel.ink/k4Pixo'},
+    {url: 'https://www.wappalyzer.com/upgraded/?utm_source=upgraded&utm_medium', shortUrl: 'https://rel.ink/k4Pcxp'}
   ])
 
-  
+fetch('https://cleanuri.com/api/v1/shorten', {
+  Method: 'POST',
+  Headers: {
+    Accept: 'application.json',
+    'Content-Type': 'application/json'
+  },
+  Body: 'url=https://fonts.google.com/specimen/Open+Sans',
+  Cache: 'default'
+}).then(response=> console.log(response))
 
   return (
     <div className='app'>
